8837d30689b98488d799f00104a2cd508b6b1262
kjell <kjell5317@gmail.com>
2022-02-05 14:13:27 +0100

    merge Birk

--- Programmierung/UB10/Aufgabe2/TrieNode.java
+++ Programmierung/UB10/Aufgabe2/TrieNode.java
@@ -1,81 +1,101 @@
 package UB10.Aufgabe2;
 
+import java.util.LinkedList;
+
 public class TrieNode {
 
-    private char letter;
-    private int value;
-    private TrieNode[] children;
+  private char letter;
+  private int value;
+  LinkedList<TrieNode> children;
+  boolean isEnd;
 
-    public TrieNode() {
-        // this.letter ist nicht definiert
-        this.value = -1;
-        this.children = new TrieNode[3];
-    }
+  public TrieNode(char letter) {
+    children = new LinkedList<TrieNode>();
+    isEnd = false;
+    setValue(-1);
+    setLetter(letter);
+  }
 
-    public TrieNode(char inLetter) {
-        this.letter = inLetter;
-        this.value = -1;
-        this.children = new TrieNode[3];
-    }
+  public TrieNode(char letter, int value) {
+    children = new LinkedList<TrieNode>();
+    isEnd = false;
+    setValue(value);
+    setLetter(letter);
+  }
 
-    // Getter und Setter
+  public int getValue() {
+    return value;
+  }
 
-    public int getLetter() {
-        return letter;
-    }
+  public void setValue(int value) {
+    this.value = value;
+  }
 
-    public int getValue() {
-        return value;
-    }
+  public char getLetter() {
+    return letter;
+  }
 
-    public void setValue(int inValue) {
-        this.value = inValue;
-    }
+  public void setLetter(char letter) {
+    this.letter = letter;
+  }
 
-    /**
-     * Adds a new child node with the given key and value to this node.
-     * 
-     * @param letter The character of this node
-     * @param value  The value associated with this node, or -1 if no value is
-     *               associated
-     * @return The new child if it could be added or if a node with the given key is
-     *         already
-     *         there, null if there are already three children
-     */
-    public TrieNode addChild(char letter, int value) {
-        TrieNode tn = find(letter);
-        if (tn != null)
-            return tn;
-        for (int idx = 0; idx < 3; idx++) {
-            if (children[idx] != null) {
-                continue;
-            }
-            tn = new TrieNode(letter);
-            tn.setValue(value);
-            children[idx] = tn;
-            return tn;
+  public TrieNode getChild(char c) {
+    if (children != null)
+      for (TrieNode eachChild : children)
+        if (eachChild.getLetter() == c)
+          return eachChild;
+    return null;
+  }
+
+  /**
+   * Adds a new child node with the given key and value to this node.
+   *
+   * @param letter The character of this node
+   * @param value  The value associated with this node, or -1 if no value is
+   *               associated
+   * @return The new child if it could be added or if a node with the given key is
+   *         already
+   *         there, null if there are already three children
+   */
+  public TrieNode addChild(char letter, int value) {
+    if (children != null) {
+      for (TrieNode eachchild : children) {
+        if (eachchild.getLetter() == letter) {
+          if (eachchild.getValue() == value) {
+            return eachchild;
+          }
         }
-        return null;
+      }
     }
-
-    /**
-     * Searches this node's direct children for a node with the given key.
-     * 
-     * @param letter The character to look for
-     * @return Returns the TrieNode with the given key if it was found, or null
-     *         otherwise
-     */
-    public TrieNode find(char letter) {
-        for (TrieNode tn : children) {
-            if (tn == null) {
-                continue;
-            } else {
-                if (tn.getLetter() == letter) {
-                    return tn;
-                }
-            }
+    children.add(new TrieNode(letter, value));
+    if (children != null) {
+      for (TrieNode eachchild : children) {
+        if (eachchild.getLetter() == letter) {
+          if (eachchild.getValue() == value) {
+            return eachchild;
+          }
         }
-        return null;
+      }
     }
+    TrieNode dummy = new TrieNode(letter, value);
+    return dummy;
+  }
 
+  /**
+   * Searches this node's direct children for a node with the given key.
+   *
+   * @param letter The character to look for
+   * @return Returns the TrieNode with the given key if it was found, or null
+   *         otherwise
+   */
+  public TrieNode find(char letter) {
+    if (children != null) {
+      for (TrieNode eachchild : children) {
+        if (eachchild.getLetter() == letter) {
+          return eachchild;
+        }
+      }
+    }
+    return null;
+  }
 }
